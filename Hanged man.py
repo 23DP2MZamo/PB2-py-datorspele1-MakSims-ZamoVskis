fff847cf67ca5f3a56070742ad19838a476e773e
max zamovsky <maxzamovsky@gmail.com>
2024-03-27 14:14:13 +0200

    Versija 1. viņa strada bet useram vajag paradit vai viņs uzvarēja vai ne

--- Hanged man.py
+++ Hanged man.py
@@ -9,64 +9,71 @@ from time import sleep
 print("Sveicinam jūs spēlē HangMan")
 print("-------------------------------------------")
 
-vardubiblioteka = ["scooter", "flower", "Latvia", "Program", "Yeat","Labdien", "Matematika", "Triangle", "Algoritms", "MarkDown", "Python", "Computer", "Linux", "Windows", "Stockholm"]
+wordDictionary = ["four", "house", "sunflower", "latvia", "Python", "hello", "music", "Heart", "espada", "algoritm", "programmmer", "RVT", "Riga", "Potato", "Apple", "Android", "Windows", "Linux", "Computer"]
 
-### Izvelēties jebkuru vārdu
-vards = random.choice(vardubiblioteka)
+### Choose a random word
+randomWord = random.choice(wordDictionary)
 
-for x in vards:
+for x in randomWord:
   print("_", end=" ")
 
 def print_hangman(wrong):
   if(wrong == 0):
+    system("cls")
     print("\n+---+")
     print("    |")
     print("    |")
     print("    |")
     print("   ===")
-  elif(wrong == 1): 
+  elif(wrong == 1):
+    system("cls")
     print("\n+---+")
     print("O   |")
     print("    |")
     print("    |")
     print("   ===")
   elif(wrong == 2):
+    system("cls")
     print("\n+---+")
     print("O   |")
     print("|   |")
     print("    |")
     print("   ===")
   elif(wrong == 3):
+    system("cls")
     print("\n+---+")
     print(" O  |")
     print("/|  |")
     print("    |")
     print("   ===")
   elif(wrong == 4):
+    system("cls")
     print("\n+---+")
     print(" O  |")
     print("/|\ |")
     print("    |")
     print("   ===")
   elif(wrong == 5):
+    system("cls")
     print("\n+---+")
     print(" O  |")
     print("/|\ |")
     print("/   |")
     print("   ===")
   elif(wrong == 6):
+    system("cls")
     print("\n+---+")
     print(" O   |")
     print("/|\  |")
     print("/ \  |")
     print("    ===")
 
-def printWord(uzzinatievardi):
+def printWord(guessedLetters):
   counter=0
   rightLetters=0
-  for char in vards:
-    if(char in uzzinatievardi):
-      print(vards[counter], end=" ")
+  for char in randomWord:
+    if(char in guessedLetters):
+      print(randomWord[counter], end=" ")
       rightLetters+=1
     else:
       print(" ", end=" ")
@@ -75,10 +82,10 @@ def printWord(uzzinatievardi):
 
 def printLines():
   print("\r")
-  for char in vards:
+  for char in randomWord:
     print("\u203E", end=" ")
 
-length_of_word_to_guess = len(vards)
+length_of_word_to_guess = len(randomWord)
 amount_of_times_wrong = 0
 current_guess_index = 0
 current_letters_guessed = []
@@ -91,14 +98,14 @@ while(amount_of_times_wrong != 6 and current_letters_right != length_of_word_to_
   ### Prompt user for input
   letterGuessed = input("\nGuess a letter: ")
   ### User is right
-  if(vards[current_guess_index] == letterGuessed):
+  if(randomWord[current_guess_index] == letterGuessed):
     print_hangman(amount_of_times_wrong)
     ### Print word
     current_guess_index+=1
     current_letters_guessed.append(letterGuessed)
     current_letters_right = printWord(current_letters_guessed)
     printLines()
-  ### User was wrong a lot of times 
+  ### User was wrong af
   else:
     amount_of_times_wrong+=1
     current_letters_guessed.append(letterGuessed)
@@ -108,60 +115,5 @@ while(amount_of_times_wrong != 6 and current_letters_right != length_of_word_to_
     current_letters_right = printWord(current_letters_guessed)
     printLines()
 
-system("cls")
-print("\n+---+")
-print("    |")
-print("    |")
-print("    |")
-print("   ===")
-sleep(0.5)
 
-system("cls")
-print("\n+---+")
-print("O   |")
-print("    |")
-print("    |")
-print("   ===")
-sleep(0.5)
-
-system("cls")
-print("\n+---+")
-print("O   |")
-print("|   |")
-print("    |")
-print("   ===")
-sleep(0.5)
-
-system("cls")
-print("\n+---+")
-print(" O  |")
-print("/|  |")
-print("    |")
-print("   ===")
-sleep(0.5)
-
-system("cls")
-print("\n+---+")
-print(" O  |")
-print("/|\ |")
-print("    |")
-print("   ===")
-sleep(0.5)
-
-system("cls")
-print("\n+---+")
-print(" O  |")
-print("/|\ |")
-print("/   |")
-print("   ===")
-sleep(0.5)
-
-system("cls")
-print("\n+---+")
-print(" O   |")
-print("/|\  |")
-print("/ \  |")
-print("    ===")
-sleep(0.5)
-
-print("GAME OVER!!")
+print("GAME OVER, Thanks for playing ;D!!")
